6abd4fd57fb7724ce57b4c48816dc88226a9174e
Franklin Crosby <obleo33@gmail.com>
2017-04-05 11:03:25 -0600

    Add 2 methods for new reminder

--- app/controllers/reminders/new.js
+++ app/controllers/reminders/new.js
@@ -1,9 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  // title: '',
+  // body: '',
+  // date: '',
+
   actions: {
-    storeIdea(model) {
-      model.save()
+  //   submitReminder() {
+  //     const reminder = this.getProperties('title', 'body', 'date')
+  //     this.store.createRecord('reminder', reminder).save().then(() => {
+  //       this.setProperties({title: '', body: '', date: ''})
+  //     })
+  //   }
+  // }
+
+    submitReminder(model) {
+      model.set('date', new Date())
+      model.save().then(()=> {
+        this.transitionToRoute('reminders.reminder', model)
+      })
     }
   }
 });
+
+
+// this.setProperties(model: {})
