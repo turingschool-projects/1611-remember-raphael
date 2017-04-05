cc9940ffef047eddf2ef6807b3aeeb0d3b26d6f8
Franklin Crosby <obleo33@gmail.com>
2017-04-05 17:17:21 -0600

    Simplify remove function to use the model that is passed in

--- app/controllers/reminders/reminder/index.js
+++ app/controllers/reminders/reminder/index.js
@@ -3,13 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     removeReminder(model) {
-      this.get('store').findRecord('reminder', model.id).then(function(reminder) {
-        reminder.deleteRecord();
-        reminder.get('isDeleted');
-        reminder.save().then(() => {
-          this.transitionToRoute('reminders.reminder.1')
-        });
-      })
+      model.deleteRecord()
+      model.save()
+      this.transitionToRoute('reminders')
     }
   }
 });
