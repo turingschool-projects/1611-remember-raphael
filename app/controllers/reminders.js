38b5fbba4788beef410032d3b515ba0d19ec0b55
Franklin Crosby <obleo33@gmail.com>
2017-04-05 18:01:55 -0600

    Add remove button from the main reminders page

---
+++ app/controllers/reminders.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    removeReminder(reminder) {
+      reminder.deleteRecord();
+      reminder.save();
+      this.transitionToRoute('reminders')
+    }
+  }
+});
