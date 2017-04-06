adc78617fb285e79202ac33c1ef8a326184f31cf
Michael Fenwick <mickyfen17@aol.com>
2017-04-05 21:36:01 -0600

    add destroyRecord and fix semicolons

--- app/controllers/reminders/reminder/index.js
+++ app/controllers/reminders/reminder/index.js
@@ -3,9 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     removeReminder(model) {
-      model.deleteRecord()
-      model.save()
-      this.transitionToRoute('reminders')
+      model.destroyRecord();
+      this.transitionToRoute('reminders');
     }
   }
 });
